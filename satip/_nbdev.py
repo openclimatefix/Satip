b4e97960cb210f7be946a768d183d027db92e6d2
AyrtonB <AyrtonBourn@Outlook.com>
2020-11-12 10:07:49 +0000

    added generated modules

--- satip/_nbdev.py
+++ satip/_nbdev.py
@@ -2,9 +2,23 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"request_access_token": "01_eumetsat.ipynb"}
+index = {"create_markdown_table": "00_utils.ipynb",
+         "set_up_logging": "00_utils.ipynb",
+         "request_access_token": "01_eumetsat.ipynb",
+         "query_data_products": "01_eumetsat.ipynb",
+         "format_dt_str": "01_eumetsat.ipynb",
+         "identify_available_datasets": "01_eumetsat.ipynb",
+         "dataset_id_to_link": "01_eumetsat.ipynb",
+         "json_extract": "01_eumetsat.ipynb",
+         "extract_metadata": "01_eumetsat.ipynb",
+         "metadata_maps": "01_eumetsat.ipynb",
+         "check_valid_request": "01_eumetsat.ipynb",
+         "DownloadManager": "01_eumetsat.ipynb",
+         "get_dir_size": "01_eumetsat.ipynb"}
 
-modules = ["eumetsat.py"]
+modules = ["utils.py",
+           "eumetsat.py",
+           "reproj.py"]
 
 doc_url = "https://AyrtonB.github.io/satip/"
 
