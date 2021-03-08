4646effb5505e181523bbc86a9e57229978a791a
Rabscuttler <laurence.watson@gmail.com>
2021-03-08 09:45:12 +0000

    Added backfill dag, using Airflow variable to store config

---
+++ dags/backfill.py
@@ -0,0 +1,87 @@
+"""
+The airflow DAG scaffold for satip.backfill.download_missing_data_pipeline
+
+Note that this docstring must contain the strings "airflow" and "DAG" for
+Airflow to properly detect it as a DAG
+See: http://bit.ly/307VMum
+
+This dag uses an Airflow variable containing the run config - 
+add it using the Airflow UI.
+Example run config:
+
+{
+  "solids": {
+    "download_missing_eumetsat_files": {
+      "inputs": {
+        "env_vars_fp": "/srv/airflow/.env",
+        "data_dir": "/srv/airflow/data/raw_bfill",
+        "metadata_db_fp": "/srv/airflow/data/EUMETSAT_metadata.db",
+        "debug_fp": "/srv/airflow/logs/EUMETSAT_download.txt",
+        "table_id": "eumetsat.metadata",
+        "project_id": "solar-pv-nowcasting",
+        "start_date": "2019-02-01T00:00:00",
+        "end_date": "2019-02-01T00:05:00"
+      }
+    },
+    "df_metadata_to_dt_to_fp_map": {
+      "inputs": {
+        "data_dir": "/srv/airflow/data/raw_bfill"
+      }
+    },
+    "reproject_datasets": {
+      "inputs": {
+        "new_coords_fp": "/srv/airflow/data/intermediate/reproj_coords_TM_4km.csv",
+        "new_grid_fp": "/srv/airflow/data/intermediate/new_grid_4km_TM.json"
+      }
+    },
+    "compress_and_save_datasets": {
+      "inputs": {
+        "zarr_bucket": "solar-pv-nowcasting-data/satellite/EUMETSAT/SEVIRI_RSS/full_extent_TM_int16",
+        "var_name": "stacked_eumetsat_data"
+      }
+    },
+    "save_metadata": {
+      "inputs": {
+        "table_id": "eumetsat.metadata",
+        "project_id": "solar-pv-nowcasting"
+      }
+    },
+    "compress_export_then_delete_raw": {
+      "inputs": {
+        "data_dir": "/srv/airflow/data/raw_bfill",
+        "compressed_dir": "/srv/airflow/data/compressed_bfill",
+        "BUCKET_NAME": "solar-pv-nowcasting-data",
+        "PREFIX": "satellite/EUMETSAT/SEVIRI_RSS/native/"
+      }
+    }
+  },
+  "storage": {
+    "filesystem": {
+      "config": {
+        "base_dir": "/srv/airflow/data"
+      }
+    }
+  }
+}
+"""
+import datetime
+from airflow.models import Variable
+from dagster_airflow.factory import make_airflow_dag
+
+backfill_config = Variable.get("backfill_config", deserialize_json=True)
+
+DEFAULT_ARGS = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime.datetime(2020, 12, 14),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+}
+
+dag, tasks = make_airflow_dag(
+    module_name="satip.backfill",
+    pipeline_name="download_missing_data_pipeline",
+    run_config=backfill_config,
+    dag_kwargs={"default_args": DEFAULT_ARGS, "max_active_runs": 1, "catchup": "False"},
+)
