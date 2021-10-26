dfd6f3b3f69f04c361d5b850b7ac5de045e3817b
Jacob Bieker <jacob@bieker.tech>
2021-10-26 14:22:38 +0100

    Update for ensuring all files are used

--- satip/intermediate.py
+++ satip/intermediate.py
@@ -9,7 +9,6 @@ import os
 import fsspec
 from pathlib import Path
 import multiprocessing
-import dask.array
 from itertools import repeat
 import xarray as xr
 
@@ -32,7 +31,8 @@ def create_or_update_zarr_with_native_files(
     compressed_native_files = list(Path(directory).rglob("*.bz2"))
     number_of_timesteps = len(compressed_native_files)
     print(number_of_timesteps)
-    if os.path.exists(zarr_path):
+    zarr_exists = os.path.exists(zarr_path)
+    if zarr_exists:
         zarr_dataset = xr.open_zarr(zarr_path, consolidated=True)
         print(zarr_dataset)
         new_compressed_files = []
@@ -48,7 +48,7 @@ def create_or_update_zarr_with_native_files(
         print(len(new_compressed_files))
     pool = multiprocessing.Pool(processes=4)
     # Check if zarr already exists
-    if not os.path.exists(zarr_path):
+    if not zarr_exists:
         # Inital zarr path before then appending
         dataset = load_native_to_dataset(
             [compressed_native_files[0], "/home/jacob/Development/Satip/tests"]
@@ -58,7 +58,7 @@ def create_or_update_zarr_with_native_files(
     for dataset in pool.imap_unordered(
         load_native_to_dataset,
         zip(
-            compressed_native_files[1:],
+            compressed_native_files[1:] if not zarr_exists else compressed_native_files,
             repeat("/home/jacob/Development/Satip/tests"),
         ),
     ):
@@ -70,14 +70,6 @@ def create_or_update_zarr_with_native_files(
             timesteps_per_chunk=temporal_chunk_size,
         )
         del dataset
-    # The number of bz2 files is the number of timesteps that exist for this dataset, just make that the dummy one
-    # cloud_mask_files = Path(directory).rglob("*.grb")
-
-    # TODO To insert in the middle, could take current Zarr, make new larger one, the isel into the middle
-    # If appending then just need to append to end, but for the middle, also allows for
-    # dummy_x = dask.array.zeros(3712, chunk=256)
-    # dummy_y = dask.array.zeros(1392, chunk=256)
-    # dummy_time = dask.array.
 
 
 create_or_update_zarr_with_native_files("/home/jacob/Development/Satip", "zarr_test.zarr")
