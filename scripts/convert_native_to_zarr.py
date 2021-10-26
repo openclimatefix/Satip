f085438788d53b3ebdf421cd933f481daae5b738
Jacob Bieker <jacob@bieker.tech>
2021-10-26 15:09:37 +0100

    Add script

---
+++ scripts/convert_native_to_zarr.py
@@ -0,0 +1,23 @@
+from satip.intermediate import create_or_update_zarr_with_native_files
+import click
+
+
+@click.command()
+@click.option(
+    "--directory",
+    "--dir",
+    default="/storage/",
+    help="The top level directory where the native files are stored",
+)
+@click.option(
+    "--zarr_path",
+    "-zarr",
+    default="eumetsat_zarr.zarr",
+    prompt="The location for the Zarr file",
+)
+def create_eumetsat_zarr(*args, **kwargs):
+    create_or_update_zarr_with_native_files(*args, **kwargs)
+
+
+if __name__ == "__main__":
+    create_eumetsat_zarr()
