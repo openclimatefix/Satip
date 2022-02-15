6393b494fc21ccedf3450153bd055e5fa026c661
Jacob Bieker <jacob@bieker.tech>
2022-02-15 21:06:46 +0000

    Don't try replacing

--- scripts/convert_old_to_new_zarr.py
+++ scripts/convert_old_to_new_zarr.py
@@ -141,7 +141,7 @@ def convert_to_new_format(dataset: xr.Dataset, hrv: bool = False, new_zarr_path:
     data_array /= 1023
     data_array = data_array.clip(min=0, max=1)
     data_array["time"] = data_array.coords["time"].dt.round("5 min").values
-    data_array = replace_osgb(data_array)
+    #data_array = replace_osgb(data_array)
     print(data_array)
     for i in range(10, len(data_array["time"].values), 10):
         save_dataset_to_zarr(
@@ -150,8 +150,8 @@ def convert_to_new_format(dataset: xr.Dataset, hrv: bool = False, new_zarr_path:
             compressor_name="jpeg-xl",
             zarr_mode="a",
             timesteps_per_chunk=1,
-            y_size_per_chunk=1536 if hrv else 512,
-            x_size_per_chunk=1536 if hrv else 512,
+            y_size_per_chunk=512,
+            x_size_per_chunk=512,
         )
 
 
@@ -162,7 +162,7 @@ def convert_to_new_format_start(dataset: xr.Dataset, hrv: bool = False, new_zarr
     data_array /= 1023
     data_array = data_array.clip(min=0, max=1)
     data_array["time"] = data_array.coords["time"].dt.round("5 min").values
-    data_array = replace_osgb(data_array)
+    #data_array = replace_osgb(data_array)
     print(data_array)
     if not os.path.exists(new_zarr_path):
         save_dataset_to_zarr(
@@ -171,8 +171,8 @@ def convert_to_new_format_start(dataset: xr.Dataset, hrv: bool = False, new_zarr
             compressor_name="jpeg-xl",
             zarr_mode="w",
             timesteps_per_chunk=1,
-            y_size_per_chunk=1536 if hrv else 512,
-            x_size_per_chunk=1536 if hrv else 512,
+            y_size_per_chunk=512,
+            x_size_per_chunk=512,
         )
 
 
@@ -197,11 +197,12 @@ def fix_non_hrv(non_name):
     convert_to_new_format_start(dataset, hrv=False, new_zarr_path=new_path)
 
 
+for _ in pool.imap_unordered(fix_non_hrv, non_hrv_names):
+    print()
+
 for _ in pool.imap_unordered(fix_hrv, hrv_names):
     print()
 
-for _ in pool.imap_unordered(fix_non_hrv, non_hrv_names):
-    print()
 
 
 def fix_hrv_full(non_name):
