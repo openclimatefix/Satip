09de58e03001293339c9690a17ba187a0cffd8c2
Jacob Bieker <jacob@bieker.tech>
2022-02-22 11:39:20 +0000

    Save out to NetCDF

--- satip/utils.py
+++ satip/utils.py
@@ -293,6 +293,117 @@ def convert_scene_to_dataarray(
 
     return dataarray
 
+def save_native_to_netcdf(list_of_native_files: list, bands: list = ["HRV","IR_016",
+                                                                     "IR_039",
+                                                                     "IR_087",
+                                                                     "IR_097",
+                                                                     "IR_108",
+                                                                     "IR_120",
+                                                                     "IR_134",
+                                                                     "VIS006",
+                                                                     "VIS008",
+                                                                     "WV_062",
+                                                                     "WV_073",], save_dir: str = "./") -> None:
+    """
+    Saves native files to NetCDF for consumer
+
+    Args:
+        list_of_native_files: List of native files to convert into a single NetCDF file
+        bands: Bands to save
+    """
+    datasets = []
+    hrv_datasets = []
+    for f in list_of_native_files:
+        if "HRV" in bands:
+            use_hrv = True
+            hrv_scaler = ScaleToZeroToOne(
+                variable_order=["HRV"], maxs=np.array([103.90016]), mins=np.array([-1.2278595])
+            )
+            hrv_scene = Scene(filenames={"seviri_l1b_native": [f]})
+            hrv_scene.load(
+                [
+                    "HRV",
+                ]
+            )
+            hrv_dataarray: xr.DataArray = convert_scene_to_dataarray(
+                hrv_scene, band="HRV", area='UK', calculate_osgb=True
+            )
+            hrv_dataarray = hrv_scaler.rescale(hrv_dataarray)
+            hrv_dataarray = hrv_dataarray.transpose("time", "y_geostationary", "x_geostationary", "variable")
+            hrv_dataset = hrv_dataarray.to_dataset(name="data")
+            hrv_datasets.append(hrv_dataset)
+
+        scaler = ScaleToZeroToOne(
+            mins=np.array(
+                [
+                    -2.5118103,
+                    -64.83977,
+                    63.404694,
+                    2.844452,
+                    199.10002,
+                    -17.254883,
+                    -26.29155,
+                    -1.1009827,
+                    -2.4184198,
+                    199.57048,
+                    198.95093,
+                ]
+            ),
+            maxs=np.array(
+                [
+                    69.60857,
+                    339.15588,
+                    340.26526,
+                    317.86752,
+                    313.2767,
+                    315.99194,
+                    274.82297,
+                    93.786545,
+                    101.34922,
+                    249.91806,
+                    286.96323,
+                ]
+            ),
+            variable_order=[
+                "IR_016",
+                "IR_039",
+                "IR_087",
+                "IR_097",
+                "IR_108",
+                "IR_120",
+                "IR_134",
+                "VIS006",
+                "VIS008",
+                "WV_062",
+                "WV_073",
+            ],
+        )
+        scene = Scene(filenames={"seviri_l1b_native": [f]})
+        dataarray: xr.DataArray = convert_scene_to_dataarray(
+            scene, band="IR_016", area='UK', calculate_osgb=True
+        )
+        dataarray = scaler.rescale(dataarray)
+        dataarray = dataarray.transpose("time", "y_geostationary", "x_geostationary", "variable")
+        dataset = dataarray.to_dataset(name="data")
+        datasets.append(dataset)
+
+    # Make one array
+    if hrv_datasets:
+        hrv_dataset = xr.concat(hrv_datasets, "time")
+    if datasets:
+        dataset = xr.concat(datasets, "time")
+
+    # Now save out to NetCDF
+    hrv_dataset.to_netcdf(os.path.join(save_dir, "hrv_latest.nc"), mode='w', compute=True)
+    dataset.to_netcdf(os.path.join(save_dir, "latest.nc"), mode='w', compute=True)
+
+    # Save as datetime now
+    now_time = datetime.datetime.utcnow().strftime("%Y%m%d%H%M")
+    hrv_dataset.to_netcdf(os.path.join(save_dir, f"hrv_{now_time}.nc"), mode='w', compute=True)
+    dataset.to_netcdf(os.path.join(save_dir, f"{now_time}.nc"), mode='w', compute=True)
+
+
+
 
 def save_dataset_to_zarr(
     dataarray: xr.DataArray,
--- scripts/app.py
+++ scripts/app.py
@@ -8,7 +8,8 @@ import pandas as pd
 from satpy import Scene
 
 from satip.eumetsat import DownloadManager
-from satip.intermediate import load_native_to_datas
+from satip.intermediate import load_native_to_dataset
+from satip.utils import save_native_to_netcdf
 
 logging.basicConfig(format="%(asctime)s %(name)s %(levelname)s:%(message)s")
 logging.getLogger("satip").setLevel(getattr(logging, os.environ.get("LOG_LEVEL", "INFO")))
@@ -37,7 +38,14 @@ logger = logging.getLogger(__name__)
     help="Where to save the zarr files",
     type=click.STRING,
 )
-def run(api_key, api_secret, save_dir):
+@click.option(
+    "--history",
+    default="1 hour",
+    envvar="HISTORY",
+    help="How much history to save",
+    type=click.STRING,
+)
+def run(api_key, api_secret, save_dir, history):
     """Run main application
     1. Get data from API, download grip files
     2. Load grib files to one Xarray Dataset
@@ -48,16 +56,14 @@ def run(api_key, api_secret, save_dir):
     # 1. Get data from API, download native files
     download_manager = DownloadManager(user_key=api_key, user_secret=api_secret, data_dir=save_dir)
     download_manager.download_date_range(
-        start_date=(pd.Timestamp().now() - pd.Timedelta("1 hour")).strftime("%Y-%m-%d-%H-%M-%S"),
+        start_date=(pd.Timestamp().now() - pd.Timedelta(history)).strftime("%Y-%m-%d-%H-%M-%S"),
         end_date=pd.Timestamp().now().strftime("%Y-%m-%d-%H-%M-%S"),
     )
 
     # 2. Load grib files to one Xarray Dataset
     native_files = list(glob.glob(os.path.join(save_dir, "*.nat")))
     # data = datahub.load_all_files()
-
-    # 3. Save to directory
-    # save(dataset=data, save_dir=save_dir)
+    save_native_to_netcdf(native_files, save_dir=save_dir)
 
     logger.info("Finished Running application.")
 
