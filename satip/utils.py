d0b6eeedbc3b4570346000c2aba2bf7f17419ff3
peterdudfield <peter.dudfield@hotmail.com>
2022-03-08 19:12:39 +0000

    more verbose logs

--- satip/utils.py
+++ satip/utils.py
@@ -616,8 +616,9 @@ def filter_dataset_ids_on_current_files(datasets, save_dir: str):
             )
         )
     if len(finished_datetimes) > 0:
+        logger.debug(finished_datetimes)
         logger.debug(
-            f"The latest already downloaded finished datetime is {max(finished_datetimes)}"
+            f"The already downloaded finished datetime are {finished_datetimes}"
         )
     else:
         logger.debug("There are no files already downloaded")
