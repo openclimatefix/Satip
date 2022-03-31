11ce0befa5afeacdeca7393a4ca02498d63587e6
peterdudfield <peter.dudfield@hotmail.com>
2022-03-31 17:56:39 +0200

    update

--- satip/app.py
+++ satip/app.py
@@ -98,7 +98,7 @@ def run(api_key, api_secret, save_dir, history, db_url: Optional[str] = None):
     if db_url is not None:
         connection = DatabaseConnection(url=db_url, base=Base_Forecast)
         with connection.get_session() as session:
-            update_latest_input_data_last_updated(session=session, component="nwp")
+            update_latest_input_data_last_updated(session=session, component="satellite")
 
     logger.info("Finished Running application.")
 
