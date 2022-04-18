61100ab1fd00c531b2a7972e556c4af7c951d197
Notger Heinz <notger.heinz@gmail.com>
2022-04-18 16:23:21 +0200

    Added test for daywise-chunked datetime-download list

--- tests/test_download.py
+++ tests/test_download.py
@@ -2,7 +2,9 @@
 import os
 import unittest
 
-from satip.download import download_eumetsat_data
+import pandas as pd
+
+from satip.download import _determine_datetimes_to_download_files, download_eumetsat_data
 from satip.utils import format_dt_str
 
 
@@ -23,7 +25,27 @@ class TestDownload(unittest.TestCase):
                 user_secret=os.environ.get("EUMETSAT_USER_SECRET"),
                 auth_filename=None,
                 number_of_processes=2,
-                product=["cloud"],
+                product=["cloud", "rss"],
                 enforce_full_days=False,
             )
         )
+
+    def test_determine_datetime_to_download_files(self):
+        """Tests correct day-wise-chunked lists.
+
+        Given an empty directory, the function is supposed to return a complete
+        list of dates, chunked into daily intervals.
+        """
+        datetimes = _determine_datetimes_to_download_files(
+            ".",
+            start_date=pd.to_datetime("2020-03-08 12:00"),
+            end_date=pd.to_datetime("2020-03-10 09:00"),
+            product_id="dummy",
+        )
+        print(datetimes)
+        print(datetimes[0])
+        print(type(datetimes[0][0]))
+        self.assertEqual(datetimes[0][0], pd.to_datetime("2020-03-08 11:59:00"))
+        self.assertEqual(datetimes[0][1], pd.to_datetime("2020-03-09 11:58:00"))
+        self.assertEqual(datetimes[1][0], pd.to_datetime("2020-03-09 11:59:00"))
+        self.assertEqual(datetimes[1][1], pd.to_datetime("2020-03-10 11:58:00"))
