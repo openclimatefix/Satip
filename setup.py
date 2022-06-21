b6aa3e951cc4b035abb1a16b751a847d840cc789
BumpVersion Action <bumpversion@github-actions>
2022-06-21 18:36:54 +0000

    Bump version: 2.9.4 → 2.9.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = (this_directory / "requirements.txt").read_text().splitlines(
 
 setup(
     name="satip",
-    version="2.9.4",
+    version="2.9.5",
     license="MIT",
     description="""Satip provides the functionality necessary for
                    retrieving, and storing EUMETSAT data""",
