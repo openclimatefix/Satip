8687ff83410f6c9456770d90bcd1246cc6f7bfb3
Jacob Bieker <jacob@bieker.tech>
2022-06-21 12:45:56 +0100

    Fix for 15-minutely names

--- satip/eumetsat.py
+++ satip/eumetsat.py
@@ -550,7 +550,7 @@ def eumetsat_filename_to_datetime(inner_tar_name):
         eumetsat_filename_to_datetime(filename)
     """
 
-    p = re.compile(r"^MSG[123]-SEVI-MSG15-0100-NA-(\d*)\.")
+    p = re.compile(r"^MSG[1234]-SEVI-MSG15-0100-NA-(\d*)\.")
     title_match = p.match(inner_tar_name)
     date_str = title_match.group(1)
     return datetime.datetime.strptime(date_str, "%Y%m%d%H%M%S")
