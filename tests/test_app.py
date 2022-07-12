494a01e7b5fb0533a08e6f0706f4a9ddf9f5eae9
Jacob Bieker <jacob@bieker.tech>
2022-07-12 12:47:08 +0100

    Refreeze time

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,6 @@ import datetime
 import glob
 import os
 import tempfile
-
 from click.testing import CliRunner
 from freezegun import freeze_time
 
@@ -14,7 +13,7 @@ from satip.app import run
 runner = CliRunner()
 
 
-#@freeze_time("2022-06-28 12:00:00")  # Date with RSS imagery
+@freeze_time("2022-06-28 12:00:00")  # Date with RSS imagery
 def test_save_to_netcdf():  # noqa 103
     user_key = os.environ.get("EUMETSAT_USER_KEY")
     user_secret = os.environ.get("EUMETSAT_USER_SECRET")
@@ -62,7 +61,7 @@ def test_save_datatailor_to_disk():  # noqa 103
         assert len(native_files) > 0
 
 
-#@freeze_time("2022-06-28 12:00:00")  # Date with RSS imagery
+@freeze_time("2022-06-28 12:00:00")  # Date with RSS imagery
 def test_save_to_netcdf_rescaled():  # noqa 103
     user_key = os.environ.get("EUMETSAT_USER_KEY")
     user_secret = os.environ.get("EUMETSAT_USER_SECRET")
