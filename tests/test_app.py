c2aa0e11374c2a6d94518e42dca991b84b061156
Jacob Bieker <jacob@bieker.tech>
2022-07-12 11:53:24 +0100

    Don't freeze time on test

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,7 +14,7 @@ from satip.app import run
 runner = CliRunner()
 
 
-@freeze_time("2022-06-28 12:00:00")  # Date with RSS imagery
+#@freeze_time("2022-06-28 12:00:00")  # Date with RSS imagery
 def test_save_to_netcdf():  # noqa 103
     user_key = os.environ.get("EUMETSAT_USER_KEY")
     user_secret = os.environ.get("EUMETSAT_USER_SECRET")
@@ -62,7 +62,7 @@ def save_datatailor_to_disk():  # noqa 103
         assert len(native_files) > 0
 
 
-@freeze_time("2022-06-28 12:00:00")  # Date with RSS imagery
+#@freeze_time("2022-06-28 12:00:00")  # Date with RSS imagery
 def test_save_to_netcdf_rescaled():  # noqa 103
     user_key = os.environ.get("EUMETSAT_USER_KEY")
     user_secret = os.environ.get("EUMETSAT_USER_SECRET")
