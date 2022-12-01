e62c45be462e674c43186c2fb372fe646c12214e
peterdudfield <peter.dudfield@hotmail.com>
2022-12-01 18:47:34 +0000

    lint

--- satip/data_store.py
+++ satip/data_store.py
@@ -4,9 +4,13 @@
 def dateset_it_to_filename(dataset_id: str, tailor_id: str, dir) -> str:
     """Change a dataset id to a file name
 
-    e.g 'MSG4-SEVI-MSG15-0100-NA-20221201161242.889000000Z-NA' to MSG4-SEVI-MSG15-0100-NA-20221201161242.889000000Z.NA
+    e.g 'MSG4-SEVI-MSG15-0100-NA-20221201161242.889000000Z-NA'
+    to MSG4-SEVI-MSG15-0100-NA-20221201161242.889000000Z.NA
 
-
+    :param dataset_id: dataset id
+    :param tailor_id: taylor id
+    :param dir: directory where to save the file
+    :return: filename
     """
 
     dataset_id = f"{dir}/{dataset_id}_{tailor_id}.NA"
