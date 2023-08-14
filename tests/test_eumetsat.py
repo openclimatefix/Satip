05b4ef31ba4a167b791e43192a14147cd21c71a8
Jacob Bieker <jacob@bieker.tech>
2023-08-14 10:48:11 +0100

    Add test

--- tests/test_eumetsat.py
+++ tests/test_eumetsat.py
@@ -3,8 +3,9 @@ import glob
 import os
 import tempfile
 from datetime import datetime, timezone, timedelta
+import pandas as pd
 
-from satip.eumetsat import DownloadManager
+from satip.eumetsat import DownloadManager, eumetsat_filename_to_datetime
 
 
 def test_download_manager_setup():
@@ -22,7 +23,14 @@ def test_download_manager_setup():
 
 def test_filename_to_datetime():
     """If there were a test here, there would also be a docstring here."""
-    pass
+    filename = "MSG4-SEVI-MSG15-0000-NA-20230814075918.739000000Z-NA"
+    expected_datetime = datetime(2023, 8, 14, 7, 59, 18)
+    actual_datetime = eumetsat_filename_to_datetime(filename)
+    assert actual_datetime == expected_datetime
+    filename = "MSG4-SEVI-MSG15-0100-NA-20230814085917.262000000Z-NA"
+    expected_datetime = datetime(2023, 8, 14, 8, 59, 17)
+    actual_datetime = eumetsat_filename_to_datetime(filename)
+    assert actual_datetime == expected_datetime
 
 
 def test_data_tailor_identify_available_datasets():
