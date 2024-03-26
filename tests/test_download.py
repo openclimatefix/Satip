d62685da7237e834a64ff9555d66570048347ef7
Azah Norbline <azahnorbline@gmail.com>
2024-03-26 17:41:55 +0100

    fixes #229 use Pytest for all tests (#242)
    * fixes #229 use Pytest for all tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,6 +1,6 @@
 """Tests for satip.download.py."""
 import os
-import unittest
+import pytest
 
 import pandas as pd
 
@@ -12,27 +12,22 @@ from satip.download import (
 from satip.utils import format_dt_str
 
 
-class TestDownload(unittest.TestCase):
+class TestDownload():
     """Test case for downloader tests."""
-
-    def setUp(self) -> None:  # noqa
-        return super().setUp()
 
     def test_download_eumetsat_data(self):  # noqa
         # Call the downloader on a very short chunk of data:
-        self.assertIsNone(
-            download_eumetsat_data(
-                download_directory=str(os.getcwd() + "/storage/"),
-                start_date=format_dt_str("2020-06-01 11:59:00"),
-                end_date=format_dt_str("2020-06-01 12:02:00"),
-                user_key=os.environ.get("EUMETSAT_USER_KEY"),
-                user_secret=os.environ.get("EUMETSAT_USER_SECRET"),
-                auth_filename=None,
-                number_of_processes=2,
-                product=["cloud", "rss"],
-                enforce_full_days=False,
-            )
-        )
+        assert download_eumetsat_data(
+            download_directory=str(os.getcwd() + "/storage/"),
+            start_date=format_dt_str("2020-06-01 11:59:00"),
+            end_date=format_dt_str("2020-06-01 12:02:00"),
+            user_key=os.environ.get("EUMETSAT_USER_KEY"),
+            user_secret=os.environ.get("EUMETSAT_USER_SECRET"),
+            auth_filename=None,
+            number_of_processes=2,
+            product=["cloud", "rss"],
+            enforce_full_days=False,
+        ) is None
 
     def test_determine_datetime_to_download_files(self):
         """Tests correct day-wise-chunked lists.
@@ -49,10 +44,10 @@ class TestDownload(unittest.TestCase):
         print(datetimes)
         print(datetimes[0])
         print(type(datetimes[0][0]))
-        self.assertEqual(datetimes[0][0], pd.to_datetime("2020-03-08 11:59:00"))
-        self.assertEqual(datetimes[0][1], pd.to_datetime("2020-03-09 11:58:00"))
-        self.assertEqual(datetimes[1][0], pd.to_datetime("2020-03-09 11:59:00"))
-        self.assertEqual(datetimes[1][1], pd.to_datetime("2020-03-10 11:58:00"))
+        assert datetimes[0][0] == pd.to_datetime("2020-03-08 11:59:00")
+        assert datetimes[0][1] == pd.to_datetime("2020-03-09 11:58:00")
+        assert datetimes[1][0] == pd.to_datetime("2020-03-09 11:59:00")
+        assert datetimes[1][1] == pd.to_datetime("2020-03-10 11:58:00")
 
     def test_get_missing_datetimes_from_list_of_files(self):
         """Tests padding of datetimes if files present are missing data for given days."""
@@ -81,4 +76,4 @@ class TestDownload(unittest.TestCase):
                 # Note that the function returns datetime-objects, but we defined the expected
                 # values as pd-datetime. Though their str-repr is different, they still pass
                 # the equality-check when compared for same dates.
-                self.assertEqual(boundary, res[i][b])
+                assert boundary == res[i][b]
