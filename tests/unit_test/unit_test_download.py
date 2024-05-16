7ee09f50a1fcf75ad19c27b55bb08f289359e828
Jacqueline James <108654780+Jacqueline-J@users.noreply.github.com>
2024-05-16 15:48:46 -0400

    Create unit_test_download.py

---
+++ tests/unit_test/unit_test_download.py
@@ -0,0 +1,65 @@
+"""Tests for satip.download.py."""
+import os
+import pytest
+
+import pandas as pd
+
+from satip.download import (
+    _determine_datetimes_to_download_files,
+    _get_missing_datetimes_from_list_of_files,
+    download_eumetsat_data,
+)
+from satip.utils import format_dt_str
+
+
+class TestDownload():
+    """Test case for downloader tests."""
+
+    def test_determine_datetime_to_download_files(self):
+        """Tests correct day-wise-chunked lists.
+
+        Given an empty directory, the function is supposed to return a complete
+        list of dates, chunked into daily intervals.
+        """
+        datetimes = _determine_datetimes_to_download_files(
+            ".",
+            start_date=pd.to_datetime("2020-03-08 12:00"),
+            end_date=pd.to_datetime("2020-03-10 09:00"),
+            product_id="dummy",
+        )
+        print(datetimes)
+        print(datetimes[0])
+        print(type(datetimes[0][0]))
+        assert datetimes[0][0] == pd.to_datetime("2020-03-08 11:59:00")
+        assert datetimes[0][1] == pd.to_datetime("2020-03-09 11:58:00")
+        assert datetimes[1][0] == pd.to_datetime("2020-03-09 11:59:00")
+        assert datetimes[1][1] == pd.to_datetime("2020-03-10 11:58:00")
+
+    def test_get_missing_datetimes_from_list_of_files(self):
+        """Tests padding of datetimes if files present are missing data for given days."""
+        # Assume we only have two files present, as something went very wrong
+        # with the download.
+        filenames = [
+            "MSG3-SEVI-MSG15-0100-NA-20190308114036.810000000Z-NA.nat",
+            "MSG3-SEVI-MSG15-0100-NA-20220308133711.810000000Z-NA.nat",
+        ]
+
+        # We then expect the function to pad this by adding missing timeranges
+        # to fill up everything from the beginning to the first day to the timestamp
+        # of the first day (case A), then everything between both files (case B)
+        # and finally fill up the rest until the end of the second day (case C).
+        expected_timestamps = [
+            (pd.to_datetime("2019-03-08 00:00:00"), pd.to_datetime("2019-03-08 11:40:00")),
+            (pd.to_datetime("2019-03-08 11:40:00"), pd.to_datetime("2022-03-08 13:37:00")),
+            (pd.to_datetime("2022-03-08 13:37:00"), pd.to_datetime("2022-03-08 23:58:00")),
+        ]
+
+        # Generate the list of missing datetime interval boundaries:
+        res = _get_missing_datetimes_from_list_of_files(filenames)
+
+        for i, interval in enumerate(expected_timestamps):
+            for b, boundary in enumerate(interval):
+                # Note that the function returns datetime-objects, but we defined the expected
+                # values as pd-datetime. Though their str-repr is different, they still pass
+                # the equality-check when compared for same dates.
+                assert boundary == res[i][b]
