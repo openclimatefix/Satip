63a689fd9e69315de8a02126857806295c1d59e7
Plomo-02 <filippo.p02@outlook.it>
2024-06-09 18:38:47 +0200

    added trace_sampler function and sentry_sdk

--- satip/app.py
+++ satip/app.py
@@ -8,10 +8,38 @@ from typing import Optional
 import click
 import pandas as pd
 import structlog
+import sentry_sdk
 
 import satip
 from satip import utils
 from satip.eumetsat import EUMETSATDownloadManager
+
+def traces_sampler(sampling_context):
+    """
+    Filter tracing for sentry logs.
+    Examine provided context data (including parent decision, if any)
+    along with anything in the global namespace to compute the sample rate
+    or sampling decision for this transaction
+    """
+
+    if os.getenv("ENVIRONMENT") == "local":
+        return 0.0
+    elif "error" in sampling_context["transaction_context"]["name"]:
+        # These are important - take a big sample
+        return 1.0
+    elif sampling_context["parent_sampled"] is True:
+        # These aren't something worth tracking - drop all transactions like this
+        return 0.0
+    else:
+        # Default sample rate
+        return 0.05
+
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    environment=os.getenv("ENVIRONMENT", "local"),
+    traces_sampler=traces_sampler
+)
 
 log = structlog.stdlib.get_logger()
 
