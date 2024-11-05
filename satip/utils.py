3e2fa49c03d57eb6547c0410b7674ef0b4ec8801
peterdudfield <peter.dudfield@hotmail.com>
2024-11-05 16:31:20 +0000

    lint

--- satip/utils.py
+++ satip/utils.py
@@ -991,7 +991,8 @@ def collate_files_into_latest(save_dir: str, use_hr_serviri: bool = False, use_i
 
         log.debug(f"Collating HRV files {filename}")
         if "s3" in save_dir:
-            files = add_backend_to_filenames(files, backend="s3")  # Added backend prefix for hrv files
+            # Added backend prefix for hrv files
+            files = add_backend_to_filenames(files, backend="s3")  
         log.debug(files)
         dataset = (
             xr.open_mfdataset(
